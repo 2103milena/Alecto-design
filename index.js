3691e9fb175c84e26e20d21da07fdf004ff6ae13
2103milena <2103milena@gmail.com>
2021-02-25 18:39:38 +0100

    latest changes

---
+++ index.js
@@ -0,0 +1,34 @@
+document.querySelector('.hamburger').addEventListener('click', () => {
+
+    document.querySelector('.mobile-version-navlist').classList.toggle('visible-mobile-version-navlist');
+    document.querySelector('.hamburger').classList.toggle('hamburger-green');
+
+})
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
